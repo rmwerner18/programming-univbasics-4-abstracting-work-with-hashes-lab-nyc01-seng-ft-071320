040e522ca354c3f1de1e6fd4dab821a9751cd390
Ryan Werner <rmwerner@umich.edu>
2020-06-17 00:56:02 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -3,13 +3,13 @@ def my_hash_creator(key, value)
 end
 
 def read_from_hash(hash, key)
-  hash[:key]
+  hash[key]
 end
 
 def update_counting_hash(hash, key)
-  if hash[:key]
-    hash[:key] += 1
+  if hash[key]
+    hash[key] += 1
   else
-    hash[:key] = 1
+    hash[key] = 1
   end
 end
